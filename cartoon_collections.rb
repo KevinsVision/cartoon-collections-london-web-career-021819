7309b49a06ad750f766a2492f167fd28cc64e6d5
Kevin Lopez Cifuentes <us.kevin@icloud.com>
2019-02-06 22:41:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,6 +16,7 @@ end
 def long_planeteer_calls(array)
   # Your code here
   array.any? { |word| word.length < 4 }
+  array.any? { |word| word.length > 4 }
 end
 
 def find_the_cheese# code an argument here
