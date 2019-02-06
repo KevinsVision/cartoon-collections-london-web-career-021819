def90da3745bb2b1cd0ddebe745d61c858521eb0
Kevin Lopez Cifuentes <us.kevin@icloud.com>
2019-02-06 22:54:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,5 +22,6 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.include?("cheddar")
+  
+  cheese_types.include?(i)
 end
